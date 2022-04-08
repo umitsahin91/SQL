0364ec8ee5c659458d75bac629d304c5f3aeaadf
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-08 17:05:45 +0300

    MsSql Database connection string

---
+++ DbConnection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DbConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSqlConnection();
+        }
+        static void GetSqlConnection()
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    System.Console.WriteLine("Bağlantı sağlandı");
+
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
